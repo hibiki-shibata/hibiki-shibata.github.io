37b00f9d71304988d95fd6ecdb77d2d5125d8164
Hibiki Shibata <hibikishibata1853@gmail.com>
2025-04-14 08:20:54 +0900

    entirely sorted

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const sections = document.querySelectorAll('.techExpertiseDivs, .educationsDivs, .experiencesDivs, .languagesDivs, .hobbiesDivs');
 
-const observer = new IntersectionObserver((entries) => {
-  console.log("ENTRIES = " + entries)
-  entries.forEach(entry => {
+const observer = new IntersectionObserver((targetDivs) => {
+  targetDivs.forEach(entry => {
     if (entry.isIntersecting) {
-      console.log("ENTRYY.TARGET = "+ entry.target)
-      console.log("ENTRYY.TARGET.CLASSLIST =  "+ entry.target.classList)
       entry.target.classList.add('visible');
-      console.log("ENTRYY.TARGET.CLASSLIST after add =  "+ entry.target.classList)
-      observer.unobserve(entry.target); // Slide only once per section
+      observer.unobserve(entry.target); 
     }
   });
 }, {
