13c7da825bf369c5047ac361aeb881c2461a8422
Hibiki Shibata <hibikishibata1853@gmail.com>
2025-04-16 21:37:00 +0900

    show description button added / javascript file mouled

---
+++ jsfiles/description.js
@@ -0,0 +1,10 @@
+function toggleDescription(button) {
+  const description = button.nextElementSibling;
+  if (description.style.display === 'none' || description.style.display === '') {
+    description.style.display = 'block';
+    button.textContent = 'Hide Description';
+  } else {
+    description.style.display = 'none';
+    button.textContent = 'Show Description';
+  }
+}
