d933103490bfb1b833ad3fef9bf9b977e4165549
Hibiki Shibata <hibikishibata1853@gmail.com>
2025-04-27 01:43:12 +0900

    darkMode button -> changible color & title names

--- jsfiles/darkmode.js
+++ jsfiles/darkmode.js
@@ -1,15 +1,17 @@
 const toggleButton = document.getElementById("darkModeIcon");
 
 toggleButton.addEventListener("click", () => {
-    
-    try {    
-        document.body.classList.toggle("dark");    
+
+    try {
+        document.body.classList.toggle("dark");
 
         // Optional: save preference in local storage
         if (document.body.classList.contains("dark")) {
             localStorage.setItem("theme", "dark");
+            changeButtonTitle("dark")
         } else {
             localStorage.setItem("theme", "light");
+            changeButtonTitle()
         }
     } catch (e) {
         console.log(e)
@@ -18,10 +20,26 @@ toggleButton.addEventListener("click", () => {
 
 );
 
+
+function changeButtonTitle(isDarkMode) {
+    const buttonTitle = document.getElementById('darkModeTitle')
+    // const savedTheme = localStorage.getItem("theme");
+
+    if (isDarkMode === "dark") {
+        buttonTitle.textContent = "Light"
+    } else {
+        buttonTitle.textContent = "Dark"
+    }
+
+}
+
 // Apply saved theme on page load
 window.addEventListener("DOMContentLoaded", () => {
     const savedTheme = localStorage.getItem("theme");
     if (savedTheme === "dark") {
         document.body.classList.add("dark");
+        changeButtonTitle(savedTheme);
     }
 });
+
+
