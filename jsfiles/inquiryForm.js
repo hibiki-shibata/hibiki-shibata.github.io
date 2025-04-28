27b365bc2f916775bf0c74565b1d908b84019ade
Hibiki Shibata <hibikishibata1853@gmail.com>
2025-04-29 06:22:48 +0900

    contact form get blank after subission

--- jsfiles/inquiryForm.js
+++ jsfiles/inquiryForm.js
@@ -7,15 +7,21 @@
 })();
 
 
-
+const inquiryForm = document.getElementById('inquiryForm')
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('inquiryForm').addEventListener('submit', function (event) {
         event.preventDefault();
         emailjs.sendForm('service_638tkkh', 'template_75qb1he', this)
             .then(() => {
-                console.log('SUCCESS!');
+                inquiryForm.querySelectorAll('input', 'textarea').forEach(el => {
+                    el.value = ''
+                })
+
+                alert('Thank you for contact:)\nYour inquiry has been sent !');
+
             }, (error) => {
+                alert('Something went wrong:/\nYour inquiry couldn\'t be sent..');
                 console.log('FAILED...', error);
             });
     });
