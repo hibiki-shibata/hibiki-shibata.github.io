e21a378f1275a64406a38f81500a4470ad56aee6
Hibiki Shibata <hibikishibata1853@gmail.com>
2025-04-29 18:41:29 +0900

    button disable after submission & textarea empty after submission

--- jsfiles/inquiryForm.js
+++ jsfiles/inquiryForm.js
@@ -8,21 +8,32 @@
 
 
 const inquiryForm = document.getElementById('inquiryForm')
+const submitButton = inquiryForm.querySelector('button[type="submit"]');
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('inquiryForm').addEventListener('submit', function (event) {
         event.preventDefault();
+
+        submitButton.disabled = true;
+        submitButton.textContent = 'Sending...';
+
         emailjs.sendForm('service_638tkkh', 'template_75qb1he', this)
             .then(() => {
-                inquiryForm.querySelectorAll('input', 'textarea').forEach(el => {
+                inquiryForm.querySelectorAll('input, textarea').forEach(el => {
                     el.value = ''
                 })
+
+                submitButton.disabled = false;
+                submitButton.textContent = 'Submit';
 
                 alert('Thank you for contact:)\nYour inquiry has been sent !');
 
             }, (error) => {
                 alert('Something went wrong:/\nYour inquiry couldn\'t be sent..');
                 console.log('FAILED...', error);
+
+                submitButton.disabled = false;
+                submitButton.textContent = 'Submit';
             });
     });
 });
