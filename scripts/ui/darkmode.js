d6ee50969dad0fb24326152f09c46826ef3ded0a
hibiki-shibata <hibiki.shibata@wolt.com>
2026-01-04 20:17:19 +0900

    Edit: default color = dark

--- scripts/ui/darkmode.js
+++ scripts/ui/darkmode.js
@@ -1,29 +1,24 @@
 const toggleButton = document.getElementById("control-bar-card__site-settings__darkmode-svg");
 
 toggleButton.addEventListener("click", () => {
-
     try {
         document.body.classList.toggle("dark");
-
         if (document.body.classList.contains("dark")) {
             localStorage.setItem("theme", "dark");
             changeButtonTitle("dark")
         } else {
             localStorage.setItem("theme", "light");
-            changeButtonTitle()
+            changeButtonTitle("light")
         }
     } catch (e) {
         console.log(e)
     }
 }
-
-);
-
+)
 
 function changeButtonTitle(isDarkMode) {
     const buttonTitle = document.getElementById('control-bar-card__site-settings__darkmode-title')
     // const savedTheme = localStorage.getItem("theme");
-
     if (isDarkMode === "dark") {
         buttonTitle.textContent = "Light"
     } else {
@@ -34,7 +29,7 @@ function changeButtonTitle(isDarkMode) {
 
 window.addEventListener("DOMContentLoaded", () => {
     const savedTheme = localStorage.getItem("theme");
-    if (savedTheme === "dark") {
+    if (savedTheme !== "light") {
         document.body.classList.add("dark");
         changeButtonTitle(savedTheme);
     }
