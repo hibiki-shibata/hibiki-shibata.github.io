897932f41de36ea7df1417dc2ba9875860f7fca6
hibiki-shibata <hibiki.shibata@wolt.com>
2026-01-06 00:50:51 +0900

    Add: mobile mebuBarBtm js

--- scripts/ui/show-menu-bar.js
+++ scripts/ui/show-menu-bar.js
@@ -1,7 +1,13 @@
 const menuBtn = document.getElementById('control-bar-card__menu-button')
+const mobileMenuBtn = document.getElementById('mobile-only-control-bar-card__menu-button')
 const sidebar = document.getElementById('control-bar-card__menu-side-bar')
 
 menuBtn.addEventListener('click', (event) => {
+    event.stopPropagation()
+    sidebar.classList.toggle('open')
+})
+
+mobileMenuBtn.addEventListener('click', (event) => {
     event.stopPropagation()
     sidebar.classList.toggle('open')
 })
