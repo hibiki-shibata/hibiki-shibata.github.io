c183104cc4dff7813bed9ad5f91742f88a01a1cb
hibiki-shibata <hibiki.shibata@wolt.com>
2026-02-04 23:22:17 +0900

    Add: react code

---
+++ my-app/src/hibiki-profile/components/titleCard.tsx
@@ -0,0 +1,35 @@
+function titleCard() {
+
+    return (
+        <>
+            <svg viewBox="0 0 1500 100">
+                <symbol id="s-text">
+                    <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle">
+                        Hibiki Shibata
+                    </text>
+                </symbol>
+                <use ></use>
+                <use ></use>
+                <use ></use>
+                <use ></use>
+                <use ></use>
+            </svg>
+
+            <h1 >HIBIKI SHIBATA</h1>
+
+            <div >
+                <div>
+                    <p data-i18n="currentPositionName">Enthusiast Of Desiging Softwares
+                    </p>
+                </div>
+                <div id="title-card__subtitle__avatar">
+                    {/* <img src="./assets/Kirbynasai-photoaidcom-cropped.png" alt="profile"> */}
+                    <div aria-hidden="true"></div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default titleCard
