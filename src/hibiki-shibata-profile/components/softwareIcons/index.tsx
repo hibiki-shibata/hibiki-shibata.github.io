215871d9321bcd54cd61c4339aff09aeb524d5e4
hibiki-shibata <hibiki.shibata@wolt.com>
2026-02-07 02:53:21 +0900

    Add: softwareIcons

---
+++ src/hibiki-shibata-profile/components/softwareIcons/index.tsx
@@ -0,0 +1,30 @@
+import Css from './css'
+import Html from './html'
+import Python from './python'
+import Kotlin from './kotlin'
+import Docker from './docker'
+import Kubernetes from './kubernetes'
+import Postgres from './postgres'
+import Git from './git'
+import Javascript from './javascript'
+import Typescript from './typescript'
+
+
+function softwareIcons() {
+    return (
+        <>
+            <Html />
+            <Css />
+            <Python />
+            <Kotlin />
+            <Docker />
+            <Kubernetes />
+            <Postgres />
+            <Git />
+            <Javascript />
+            <Typescript />
+        </>
+    )
+}
+
+export default softwareIcons
