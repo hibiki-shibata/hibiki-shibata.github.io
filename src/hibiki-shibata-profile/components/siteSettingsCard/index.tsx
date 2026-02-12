5e872f31eb529aaef27320025f8ec02ac150160d
hibiki-shibata <hibiki.shibata@wolt.com>
2026-02-12 21:42:29 +0900

    Add: twitch icon

--- src/hibiki-shibata-profile/components/siteSettingsCard/index.tsx
+++ src/hibiki-shibata-profile/components/siteSettingsCard/index.tsx
@@ -6,25 +6,26 @@ import SocialIcons from "../socialIcons"
 function siteSettingsCard() {
     return (
         <>
-            <div className="flex flex-row items-center justify-between px-10 py-2 bg-blue-950 text-xs gap-5">
-                <div className="sm:flex hidden">
+            {/* <div className="flex flex-row items-center justify-between px-10 py-2 bg-blue-950 text-xs gap-5"> */}
+            {/* <div className="flex flex-col lg:flex-row items-center justify-between px-10 py-2 bg-blue-950 text-xs gap-5">
+                <div className="flex hidden">
                     <MenuBar />
-                </div>
-                <div className="flex gap-3 sm:flex-wrap ">
-                    <SocialIcons />
-                </div>
-                <div className="sm:flex gap-2 hidden">
+                </div> */}
+            <div className="flex flex-wrap  justify-center gap-2">
+                <SocialIcons />
+                {/* </div>
+                <div className="flex gap-2 hidden">
                     <DarkModeToggle />
                     <LanguageToggle />
-                </div>
+                </div> */}
             </div>
-            <div className="sm:hidden gap-2 flex items-center justify-between px-10 py-2 bg-blue-900 text-xs">
+            {/* <div className="lg:hidden gap-2 flex items-center justify-between px-10 py-2 bg-blue-900 text-xs">
                 <MenuBar />
                 <div className="flex gap-2">
                     <DarkModeToggle />
                     <LanguageToggle />
                 </div>
-            </div>
+            </div> */}
         </>)
 }
 
