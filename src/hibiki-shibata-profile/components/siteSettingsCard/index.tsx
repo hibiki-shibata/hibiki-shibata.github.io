cfe831086ee535deb0f8c0296f258149aacd16f4
hibiki-shibata <hibikishibata1853@gmail.com>
2026-07-14 17:20:55 -0500

    Add: disabledHint to language and color theme button

--- src/hibiki-shibata-profile/components/siteSettingsCard/index.tsx
+++ src/hibiki-shibata-profile/components/siteSettingsCard/index.tsx
@@ -2,6 +2,7 @@ import DarkModeToggle from "./darkmodeToggle"
 import LanguageToggle from "./languageToggle"
 import MenuBar from "./menuBar"
 import SocialIcons from "../socialIcons"
+import DisabledHint from "../../shared/disabledHint"
 
 function siteSettingsCard() {
     return (
@@ -14,10 +15,12 @@ function siteSettingsCard() {
                 <div className="flex flex-wrap justify-center gap-2">
                     <SocialIcons />
                 </div>
-                <div className="lg:flex gap-2 hidden">
-                    <DarkModeToggle />
-                    <LanguageToggle />
-                </div>
+                <DisabledHint>
+                    <div className="lg:flex gap-2 hidden">
+                        <DarkModeToggle />
+                        <LanguageToggle />
+                    </div>
+                </DisabledHint>
             </div>
             <div className="lg:hidden flex items-center justify-between px-10 py-2 bg-blue-900 text-xs">
                 <MenuBar />
