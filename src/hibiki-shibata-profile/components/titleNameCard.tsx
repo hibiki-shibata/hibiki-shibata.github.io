f7719755f310797b44d589a444c41b5aa3b1d47c
hibiki-shibata <hibikishibata1853@gmail.com>
2026-08-08 20:37:28 +0900

    creating branch

--- src/hibiki-shibata-profile/components/titleNameCard.tsx
+++ src/hibiki-shibata-profile/components/titleNameCard.tsx
@@ -8,23 +8,9 @@ function titleNameCard() {
                 <SiteSettingsCard />
             </div>
             <div className="bg-gradient-to-r from-blue-950 to-blue-400 justify-center py-10 text-2xl flex flex-col gap-10">
-                {/* <svg viewBox="0 0 1500 100">
-                <symbol id="s-text">
-                    <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle">
-                        Hibiki Shibata
-                    </text>
-                </symbol>
-                <use ></use>
-                <use ></use>
-                <use ></use>
-                <use ></use>
-                <use ></use>
-            </svg> */}
                 <h1 >HIBIKI SHIBATA</h1>
-
                 <p>Enthusiast Of Softwares Designs
                 </p>
-                {/* <img src="./assets/Kirbynasai-photoaidcom-cropped.png" alt="profile"> */}
             </div>
 
         </>
